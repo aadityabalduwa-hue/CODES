9570a04a102d81ce4600e717d84b47bf2b531edc
aadityabalduwa-hue <aadityabalduwa@gmail.com>
2026-05-28 20:33:14 +0530

    Add files via upload

---
+++ exp4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define FALSE -1
+#define NULL 0
+#define TRUE 1
+int main(){
+    if(NULL){
+        printf("null");
+    }
+    else if(FALSE)
+    {
+        printf("TRUE");
+    }
+    else {
+        printf("FALSE");
+
+    }
+    return 0;
+
+}
