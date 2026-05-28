65c3c937bf90b14c11f07aac979b991d5e7fcffb
aadityabalduwa-hue <aadityabalduwa@gmail.com>
2026-05-28 20:38:35 +0530

    Add files via upload

---
+++ exp5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[8]="if block";
+    if(str=="if block"){
+        printf("if block is executed");
+    }
+    else {
+        printf("else block is executed");
+    }
+  
+    return 0;
+
+}
---
+++ exp6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[]="ashish\n";
+    if(printf("%s",str)){
+        printf("inside if block");
+    }
+    else {
+        printf("inside else block");
+    }
+  
+    return 0;
+
+}
